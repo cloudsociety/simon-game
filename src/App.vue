7d2d5e6956872df86a101a4ecedadee171e99100
David Cross (MBP) <david@cloudsociety.com>
2017-07-03 17:00:49 +0800

    finished game logic

--- src/App.vue
+++ src/App.vue
@@ -21,43 +21,48 @@
       <circle class="shadow" cx="183" cy="183" r="150"/>
       <circle class="base" cx="175" cy="175.027" r="150"/>
 
-      <path @click="addLight(1);addSound(1)" class="btn btn--green btn--1" d="M40.205 167.527C43.96 98.94 98.913 43.987 167.5 40.232v75.26c-27.14 3.384-48.65 24.895-52.036 52.035h-75.26z"/>
-      <path @click="addLight(2);addSound(2)" class="btn btn--red btn--2" d="M182.5 40.232c68.587 3.756 123.54 58.708 127.295 127.295h-75.26c-3.384-27.14-24.895-48.65-52.035-52.035v-75.26z"/>
-      <path @click="addLight(3);addSound(3)" class="btn btn--blue btn--3" d="M309.795 182.527c-3.755 68.588-58.708 123.54-127.295 127.296v-75.26c27.14-3.384 48.65-24.895 52.036-52.036h75.26z"/>
-      <path @click="addLight(4);addSound(4)" class="btn btn--yellow btn--4" d="M167.5 309.795C98.913 306.04 43.96 251.087 40.205 182.5h75.26c3.384 27.14 24.895 48.65 52.035 52.036v75.26z"/>
+      <path @click="humanPlay(1)" class="btn btn--green btn--1" d="M40.205 167.527C43.96 98.94 98.913 43.987 167.5 40.232v75.26c-27.14 3.384-48.65 24.895-52.036 52.035h-75.26z"/>
+      <path @click="humanPlay(2)" class="btn btn--red btn--2" d="M182.5 40.232c68.587 3.756 123.54 58.708 127.295 127.295h-75.26c-3.384-27.14-24.895-48.65-52.035-52.035v-75.26z"/>
+      <path @click="humanPlay(3)" class="btn btn--blue btn--3" d="M309.795 182.527c-3.755 68.588-58.708 123.54-127.295 127.296v-75.26c27.14-3.384 48.65-24.895 52.036-52.036h75.26z"/>
+      <path @click="humanPlay(4)" class="btn btn--yellow btn--4" d="M167.5 309.795C98.913 306.04 43.96 251.087 40.205 182.5h75.26c3.384 27.14 24.895 48.65 52.035 52.036v75.26z"/>
 
-      <g class="counter">
-        <circle class="center" cx="175" cy="175.027" r="45"/>
-        <g class="counter--pieces counter--pieces__13">
-          <path class="tens-2" d="M166.73 162.59l2.005 2.292-2.52 2.29h-14.89l-2.063-2.29 2.577-2.29z"/>
-          <path class="tens-01" d="M169.823 165.455l1.03 1.145-1.66 16.036c-.535 1.528-1.585 2.482-3.15 2.864l-1.26-4.582 1.26-12.027 3.78-3.435z"/>
-          <path class="tens-02" d="M164.897 185.5h-17.18c-1.49-.382-2.35-1.336-2.58-2.864l4.755-1.718h13.745l1.26 4.582zM145.253 181.49l1.546-14.89 1.26-1.145 3.092 3.436-1.145 10.883zM167.017 148.845h-13.69l-4.41-1.718c.536-1.527 1.586-2.482 3.15-2.863h17.126l-2.176 4.58z"/>
-          <path class="tens-0" d="M153.157 149.99l-1.146 10.883-3.836 3.436-.974-1.146 1.547-14.89z"/>
-          <path class="tens-012" d="M166.902 160.873l1.26-12.028 2.176-4.58c1.49.38 2.35 1.335 2.578 2.862l-1.66 16.037-1.32 1.145-3.034-3.437z"/>
-          <path class="ones-2345689" d="M196.508 162.59l2.005 2.292-2.52 2.29h-14.89l-2.063-2.29 2.577-2.29z"/>
-          <path class="ones-013456789" d="M199.6 165.455l1.032 1.145-1.66 16.036c-.536 1.528-1.586 2.482-3.15 2.864l-1.26-4.582 1.26-12.027 3.78-3.435z"/>
-          <path class="ones-0235689" d="M194.675 185.5h-17.18c-1.49-.382-2.35-1.336-2.58-2.864l4.755-1.718h13.745l1.26 4.582z"/>
-          <path class="ones-0268" d="M175.03 181.49l1.547-14.89 1.26-1.145 3.093 3.436-1.145 10.883z"/>
-          <path class="ones-02356789" d="M196.795 148.845h-13.69l-4.41-1.718c.536-1.527 1.586-2.482 3.15-2.863h17.126l-2.175 4.58z"/>
-          <path class="ones-0456789" d="M182.935 149.99l-1.146 10.883-3.838 3.436-.974-1.146 1.547-14.89z"/>
-          <path class="ones-01234789" d="M196.68 160.873l1.26-12.028 2.176-4.58c1.49.38 2.35 1.335 2.578 2.862l-1.66 16.037-1.32 1.145-3.034-3.437z"/>
+      <g v-if="currentLevel > 0">
+        <g class="counter">
+          <circle class="center" cx="175" cy="175.027" r="45"/>
+          <g class="counter--pieces" :class="'counter--pieces__' + currentLevel">
+            <path class="tens-2" d="M166.73 162.59l2.005 2.292-2.52 2.29h-14.89l-2.063-2.29 2.577-2.29z"/>
+            <path class="tens-01" d="M169.823 165.455l1.03 1.145-1.66 16.036c-.535 1.528-1.585 2.482-3.15 2.864l-1.26-4.582 1.26-12.027 3.78-3.435z"/>
+            <path class="tens-02" d="M164.897 185.5h-17.18c-1.49-.382-2.35-1.336-2.58-2.864l4.755-1.718h13.745l1.26 4.582zM145.253 181.49l1.546-14.89 1.26-1.145 3.092 3.436-1.145 10.883zM167.017 148.845h-13.69l-4.41-1.718c.536-1.527 1.586-2.482 3.15-2.863h17.126l-2.176 4.58z"/>
+            <path class="tens-0" d="M153.157 149.99l-1.146 10.883-3.836 3.436-.974-1.146 1.547-14.89z"/>
+            <path class="tens-012" d="M166.902 160.873l1.26-12.028 2.176-4.58c1.49.38 2.35 1.335 2.578 2.862l-1.66 16.037-1.32 1.145-3.034-3.437z"/>
+            <path class="ones-2345689" d="M196.508 162.59l2.005 2.292-2.52 2.29h-14.89l-2.063-2.29 2.577-2.29z"/>
+            <path class="ones-013456789" d="M199.6 165.455l1.032 1.145-1.66 16.036c-.536 1.528-1.586 2.482-3.15 2.864l-1.26-4.582 1.26-12.027 3.78-3.435z"/>
+            <path class="ones-0235689" d="M194.675 185.5h-17.18c-1.49-.382-2.35-1.336-2.58-2.864l4.755-1.718h13.745l1.26 4.582z"/>
+            <path class="ones-0268" d="M175.03 181.49l1.547-14.89 1.26-1.145 3.093 3.436-1.145 10.883z"/>
+            <path class="ones-02356789" d="M196.795 148.845h-13.69l-4.41-1.718c.536-1.527 1.586-2.482 3.15-2.863h17.126l-2.175 4.58z"/>
+            <path class="ones-0456789" d="M182.935 149.99l-1.146 10.883-3.838 3.436-.974-1.146 1.547-14.89z"/>
+            <path class="ones-01234789" d="M196.68 160.873l1.26-12.028 2.176-4.58c1.49.38 2.35 1.335 2.578 2.862l-1.66 16.037-1.32 1.145-3.034-3.437z"/>
+          </g>
+        </g>
+        <g class="btn btn--stop" @click="clearGame">
+          <path class="center--semi" d="M217.072 191.027c-6.45 16.954-22.854 29-42.072 29-19.218 0-35.62-12.046-42.072-29h84.144z"/>
+          <path class="stop--icon" d="M166.288 196.815h17.424v17.424h-17.424z"/>
         </g>
       </g>
-      <g class="btn btn--play" @click="startGame">
-        <circle class="center" cx="175" cy="175.027" r="45"/>
-        <path class="play--icon" d="M163.035 140.9V184l33.93-21.55z"/>
-      </g>
-      <g class="btn btn--stop" @click="clicked('stop')">
-        <path class="center--semi" d="M217.072 191.027c-6.45 16.954-22.854 29-42.072 29-19.218 0-35.62-12.046-42.072-29h84.144z"/>
-        <path class="stop--icon" d="M166.288 196.815h17.424v17.424h-17.424z"/>
-      </g>
-      <g class="btn btn--strict" @click="clicked('strict')">
-        <path class="center--semi" d="M217.072 191.027c-6.45 16.954-22.854 29-42.072 29-19.218 0-35.62-12.046-42.072-29h84.144z"/>
-        <g class="strict--text">
-          <path d="M155.907 201.51l.573.654-.72.655h-4.255l-.59-.656.737-.654zM156.79 202.328l.295.327-.474 4.582c-.152.437-.452.71-.9.818l-.36-1.31.36-3.435 1.08-.982zM155.383 208.055h-4.91c-.424-.11-.67-.38-.735-.818l1.358-.49h3.927l.36 1.308zM152.03 197.91l-.33 3.11-1.095.98-.278-.326.442-4.255zM152.078 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238zM162.19 202.328l.59.655-.443 4.254c-.153.437-.453.71-.9.818l-.36-1.31.392-3.762.72-.655zM161.65 201.346l.36-3.436h1.31l-.36 3.436-.738.655zM160.586 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238zM172.923 201.51l.573.654-.72.655h-4.255l-.588-.656.736-.654zM168.816 203.146h.98l3.814 4.255c-.262.437-.72.655-1.374.655l-3.52-3.927.1-.982zM168.472 203.31l-.36 3.436-.622 1.31c-.425-.11-.67-.382-.736-.82l.474-4.58.36-.328.884.982zM173.005 197.583h-3.91l-1.26-.49c.152-.438.452-.71.9-.82h4.892l-.622 1.31zM172.972 201.02l.36-3.437.622-1.31c.425.11.67.382.736.82l-.474 4.58-.377.328-.868-.98zM169.045 197.91l-.328 3.11-1.096.98-.277-.326.442-4.255zM178.535 201.02l.36-3.437.62-1.31c.427.11.672.382.738.82l-.49 4.58-.36.328-.868-.98zM179.385 202.164l.295.328-.475 4.582c-.163.436-.47.71-.916.818l-.345-1.31.36-3.436 1.08-.982zM189.906 208.055h-5.4c-.425-.11-.67-.38-.736-.818l1.358-.49h4.418l.36 1.308zM183.803 206.91l.44-4.255.36-.327.885.982-.327 3.11zM190.512 197.583h-4.402l-1.26-.49c.153-.438.453-.71.9-.82h5.383l-.62 1.31zM186.06 197.91l-.327 3.11-1.096.98-.278-.326.44-4.255zM196.22 202.328l.59.655-.44 4.254c-.154.437-.454.71-.9.818l-.36-1.31.39-3.762.72-.655zM195.68 201.346l.36-3.436h1.31l-.36 3.436-.736.655zM194.618 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238z"/>
+      <g v-else>
+        <g class="btn btn--play" @click="startGame">
+          <circle class="center" cx="175" cy="175.027" r="45"/>
+          <path class="play--icon" d="M163.035 140.9V184l33.93-21.55z"/>
+        </g>
+        <g class="btn btn--strict" :class="{'btn--strict__on': strictMode}" @click="strictMode = !strictMode">
+          <path class="center--semi" d="M217.072 191.027c-6.45 16.954-22.854 29-42.072 29-19.218 0-35.62-12.046-42.072-29h84.144z"/>
+          <g class="strict--text">
+            <path d="M155.907 201.51l.573.654-.72.655h-4.255l-.59-.656.737-.654zM156.79 202.328l.295.327-.474 4.582c-.152.437-.452.71-.9.818l-.36-1.31.36-3.435 1.08-.982zM155.383 208.055h-4.91c-.424-.11-.67-.38-.735-.818l1.358-.49h3.927l.36 1.308zM152.03 197.91l-.33 3.11-1.095.98-.278-.326.442-4.255zM152.078 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238zM162.19 202.328l.59.655-.443 4.254c-.153.437-.453.71-.9.818l-.36-1.31.392-3.762.72-.655zM161.65 201.346l.36-3.436h1.31l-.36 3.436-.738.655zM160.586 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238zM172.923 201.51l.573.654-.72.655h-4.255l-.588-.656.736-.654zM168.816 203.146h.98l3.814 4.255c-.262.437-.72.655-1.374.655l-3.52-3.927.1-.982zM168.472 203.31l-.36 3.436-.622 1.31c-.425-.11-.67-.382-.736-.82l.474-4.58.36-.328.884.982zM173.005 197.583h-3.91l-1.26-.49c.152-.438.452-.71.9-.82h4.892l-.622 1.31zM172.972 201.02l.36-3.437.622-1.31c.425.11.67.382.736.82l-.474 4.58-.377.328-.868-.98zM169.045 197.91l-.328 3.11-1.096.98-.277-.326.442-4.255zM178.535 201.02l.36-3.437.62-1.31c.427.11.672.382.738.82l-.49 4.58-.36.328-.868-.98zM179.385 202.164l.295.328-.475 4.582c-.163.436-.47.71-.916.818l-.345-1.31.36-3.436 1.08-.982zM189.906 208.055h-5.4c-.425-.11-.67-.38-.736-.818l1.358-.49h4.418l.36 1.308zM183.803 206.91l.44-4.255.36-.327.885.982-.327 3.11zM190.512 197.583h-4.402l-1.26-.49c.153-.438.453-.71.9-.82h5.383l-.62 1.31zM186.06 197.91l-.327 3.11-1.096.98-.278-.326.44-4.255zM196.22 202.328l.59.655-.44 4.254c-.154.437-.454.71-.9.818l-.36-1.31.39-3.762.72-.655zM195.68 201.346l.36-3.436h1.31l-.36 3.436-.736.655zM194.618 197.583l-1.26-.49c.153-.438.453-.71.9-.82h5.22c.425.11.67.382.736.82l-1.358.49h-4.238z"/>
+          </g>
         </g>
       </g>
     </svg>
+    <!-- <button @click="currentLevel=20">to 20</button> -->
   </div>
 </template>
 
@@ -69,31 +74,38 @@ var synth = new Tone.Synth().toMaster();
 export default {
   data () {
     return {
-      // msg: 'Welcome to Your Vue.js App',
+      currentLevel: 0,
+      levelArray: [],
+      playerArray:[],
       gameArray: [],
-      sounds: ['E3','A4','E4','C4']
+      strictMode: false,
+      sounds: ['E3','A4','E4','C4'],
+      delayTimer: '',
+      intervalTimer: ''
     }
   },
   methods: {
-    clicked(colour){
-      console.log(colour);
-    },
     startGame(){
       var sampleArray = [1,2,3,4];
       var currentSample;
       sampleArray = _.sampleSize(sampleArray, 3);
       do {
-        var lastItem = sampleArray.slice(-1);
-        console.log(lastItem);
-        var secondLastItem = sampleArray.slice(-2,-1);
-        console.log(secondLastItem);
+        var lastItems = sampleArray.slice(-3).join('');
         currentSample = _.random(1,4);
-        if (lastItem !== secondLastItem || lastItem !== currentSample) {
+        if (lastItems !== _.repeat(''+currentSample, 3)) {
           sampleArray.push(currentSample);
         }
       } while (sampleArray.length < 20);
-      console.log(sampleArray);
       this.gameArray = sampleArray;
+      this.currentLevel = 1;
+    },
+    clearGame(){
+      this.currentLevel= 0;
+      this.levelArray= [];
+      this.playerArray=[];
+      this.gameArray= [];
+      clearInterval(this.intervalTimer);
+      clearInterval(this.delayTimer);
     },
     addLight(num){
       var el = document.querySelector(`.btn--${num}`);
@@ -109,22 +121,100 @@ export default {
           el.classList.remove(className);
         else
           el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-      },500);
+      },250);
     },
     addSound(num){
-      synth.triggerAttackRelease(this.sounds[num-1], '16n');
+      synth.triggerAttackRelease(this.sounds[num-1], '16n', '+0.1');
+    },
+    humanPlay(num){
+      this.addLight(num);
+      this.addSound(num);
+      this.checkPlay(num);
+    },
+    checkPlay(num){
+      var playLength = this.playerArray.length;
+      console.log(this.playerArray);
+      console.log(this.levelArray);
+      if (this.levelArray[playLength] === num && this.levelArray.length-1 !== playLength) {
+        console.log('correct');
+        this.playerArray.push(num);
+      } else if (this.levelArray[playLength] === num && this.levelArray.length-1 === playLength) {
+        console.log('two');
+        this.playerArray.push(num);
+        this.currentLevel === 20 ? this.winMsg() : this.currentLevel++;
+      } else if (this.strictMode) {
+        console.log('strict');
+        this.errorMsg();
+        this.delayTimer = setTimeout(() => {
+          this.currentLevel= 0;
+          this.levelArray= [];
+          this.playerArray=[];
+          this.gameArray= [];
+
+          this.startGame();
+        },700);
+      } else {
+        console.log('nope');
+        this.errorMsg();
+        this.delayTimer = setTimeout(() => {
+          this.samplePlay();
+          this.playerArray = [];
+        },700);
+      }
+    },
+    samplePlay(){
+      var seqNum = 0;
+      var speed = 700;
+      if (this.currentLevel > 12) {
+        speed = 300;
+      } else if (this.currentLevel > 7) {
+        speed = 500;
+      }
+      this.delayTimer = setTimeout(() => {
+        this.intervalTimer = setInterval(() => {
+          if (seqNum === this.levelArray.length) {clearInterval(this.intervalTimer);return};
+          var num = this.levelArray[seqNum];
+          this.addSound(num);
+          this.addLight(num);
+          seqNum++;
+        },speed); // speed should be 300, 500, 700 (easy to hard)
+      },700);
     },
     errorMsg(){
-      var errorSeq = new Tone.Sequence((time, note) => {
-        synth.triggerAttackRelease(this.sounds[note], '32n', time);
-        this.addLight(note+1);
-      }, [0,1,2,3],'32n').start(0);
-      errorSeq.loop = 1;
-      Tone.Transport.start("+0.1");
-      setTimeout(() => {
-        Tone.Transport.stop();
-
-      },500);
+      var seqNum = 0;
+      var errSeq = [1,2,3,4];
+      this.intervalTimer = setInterval(() => {
+        if (seqNum === 4) {clearInterval(this.intervalTimer);return};
+        var num = errSeq[seqNum];
+        this.addSound(num);
+        this.addLight(num);
+        seqNum++;
+      },50);
+    },
+    winMsg(){
+      var seqNum = 0;
+      var winSeq = [1,2,3,4,4,3,2,1];
+      this.intervalTimer = setInterval(() => {
+        if (seqNum === 8) {
+          this.delayTimer = setTimeout(() => {
+            this.clearGame();
+          },500);
+          return;
+        };
+        var num = winSeq[seqNum];
+        this.addSound(num);
+        this.addLight(num);
+        seqNum++;
+      },50);
+    }
+  },
+  watch: {
+    currentLevel(value){
+      if (value !== 0) {
+        this.playerArray = [];
+        this.levelArray = this.gameArray.slice(0,value);
+        this.samplePlay();
+      }
     }
   }
 }
@@ -135,13 +225,9 @@ $steel: #666666;
 $fuscous-gray: #3b3b3b; //dark grey
 $magnesium: #b3b3b3; // light grey
 $chelsea-cucumber: #919956; // green
-$mindaro: #dae681;
 $deep-chestnut: #b35049; // red
-$bittersweet: #ff7368;
 $marigold: #b3873d; // yellow
-$golden-tainoi: #ffc157;
 $paradiso: #467f80; // blue
-$aquamarine: #77d7d9;
 
 $base-colors: $chelsea-cucumber, $deep-chestnut, $paradiso, $marigold;
 
@@ -186,34 +272,32 @@ svg{
   }
 
   &--play{
-    // display: none;
     .center{fill: $magnesium;}
-    .play--icon{fill:#666;}
+    .play--icon{fill:$steel;}
 
     &:hover{
-      filter:none;
       .play--icon{fill:#fff;}
     }
   }
 
   &--strict{
-    display: none;
-    .center--semi{fill:#666;}
+    .center--semi{fill:$steel;}
     .strict--text{fill:$magnesium}
 
     &:hover{
-      filter:none;
+      .strict--text{fill:#fff;}
+    }
+
+    &__on{
       .strict--text{fill:#fff;}
     }
   }
 
   &--stop{
-    // display: none;
-    .center--semi{fill:#666;}
+    .center--semi{fill:$steel;}
     .stop--icon{fill:$magnesium}
 
     &:hover{
-      filter:none;
       .stop--icon{fill:#fff;}
     }
   }
@@ -221,7 +305,6 @@ svg{
 }
 
 .counter {
-  display: none;
   .center {
     fill: $magnesium;
   }
@@ -244,7 +327,5 @@ svg{
     }
   }
 }
-
-
 
 </style>
